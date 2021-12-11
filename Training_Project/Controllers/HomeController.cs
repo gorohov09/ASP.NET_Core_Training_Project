1da8399c81518e002ee33472efe45d3053f8ce6b
gorohhovv <andrej.gorokhoff2017@yandex.ru>
2021-12-11 16:03:18 +0300

    Добавление первого и главного контролера

---
+++ Training_Project/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Training_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return Content("Данные из первого контроллера");
+        }
+    }
+}
--- Training_Project/Program.cs
+++ Training_Project/Program.cs
@@ -1,11 +1,30 @@
 var builder = WebApplication.CreateBuilder(args);
+
+//Получили сервисы
+var services = builder.Services;
+services.AddControllersWithViews(); // Подключили структуру MVC
+
 var app = builder.Build();
 
 // Загрузка информации из файла конфигурации
-var configuration = app.Configuration;
+//var configuration = app.Configuration;
 //var greetings = configuration["CustomGreetings"];
 
-app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+//Конвейер обработки входного соединения
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage(); //Инструмент, который позволяет перехватывать все ошибки
+}
+
+app.UseRouting(); //Добавление системы маршрутизации
+
+//app.MapGet("/", () => app.Configuration["CustomGreetings"]);
+app.MapGet("/throw", () => { throw new ApplicationException("Ошибка в программе!"); });
+
+app.MapDefaultControllerRoute(); //Добавляем обработку входящих соединений системой MVC
+
+
 
 app.Run();
 
